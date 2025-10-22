df24f3725e373820fbbefd34a3af560a72e75337
= <=>
2025-10-22 20:56:33 +0300

    setup net markdown readme

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "NET_MARKDOWN_README.md").read_text()
+
 setup(
     name="crud_jt",
     version="1.0.0.beta.0",
-    description="A simple example library",
+    description="Simlifies user session. Login / Logout / Authorization",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Vlad Akymov",
     author_email="support@crudjt.com",
     packages=find_packages(),
